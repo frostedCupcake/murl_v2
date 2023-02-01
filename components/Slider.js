b0aee5568e3172fb62a9b067458ca33214eb0fe7
jaswanth <jaswanthbeere@gmail.com>
2023-02-01 19:50:21 +0530

    small fix

--- components/Slider.js
+++ components/Slider.js
@@ -1,23 +1,23 @@
 'use client';
+
 import React from 'react';
-import styles from '../styles';
 import { motion } from 'framer-motion';
+import styles from '../styles';
+
 import { staggerContainer } from '../utils/motion';
 
-const Slider = () => {
-  return (
-    <section className={`${styles.yPaddings} sm:pl-16 pl-6 mx-auto`}>
-      <motion.div
-        variants={staggerContainer}
-        initial="hidden"
-        whileInView="show"
-        viewport={{ once: true }}
-        className={`${styles.innerWidth} mx-auto flex flex-col`}
-      >
-        Slider
-      </motion.div>
-    </section>
-  );
-};
+const Slider = () => (
+  <section className={`${styles.yPaddings} sm:pl-16 pl-6 mx-auto`}>
+    <motion.div
+      variants={staggerContainer}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: true }}
+      className={`${styles.innerWidth} mx-auto flex flex-col`}
+    >
+      Slider
+    </motion.div>
+  </section>
+);
 
 export default Slider;
